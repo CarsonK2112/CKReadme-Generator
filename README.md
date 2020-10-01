2b907a83a359d9a91158190c114c07db7075c485
Red Dragon <carsonkeller1995@hotmail.com>
2020-09-30 21:54:39 -0500

    Final commit with finished app

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 [Project Readme](#project-readme) 
 [Description](#description) 
-[Table of Contents?](#table-of-contents?) 
 [Installation](#installation) 
 [Functionality](#functionality) 
 [GitHub username](#github-username) 
+[Contributions](#contributions) 
+[Tests](#tests) 
+[Questions](#questions) 
 # ## Project Readme<br> 
-Project<br> 
+Readme Generator<br> 
 ## Description<br> 
-It's a project<br> 
-## Table of Contents?<br> 
-Table of Contents<br> 
+This is a project that generates a readme based upon command line input utilizing javascript.<br> 
 ## Installation<br> 
-It installs<br> 
+This process can be cloned from a github repository onto any machine and the command line questions can be changed to suit the means of the user.<br> 
 ## Functionality<br> 
-It functions<br> 
+This project functions through the usage of javascript code which generates the command line questions and inputs the feedback into the completed readme file.<br> 
 ## GitHub username<br> 
 CarsonK2112<br> 
+[![Generic badge](https://img.shields.io/badge/License-Test-GREEN.svg)](https://shields.io/)## Contributions<br> 
+The instructor and teaching assistants of this cohort as well as extensive google searches were instrumental in the completion of this project.<br> 
+## Tests<br> 
+Clone the github repository and type in node index to bring up the questions. Console logs can be inserted into the code to better analyze code feedback.<br> 
+## Questions<br> 
+Testemail@generichostsite.com, I can also be reached via Facebook and Linkedin.<br> 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
-const axios = require("axios")
 
 // array of questions for user
 const questions = [
@@ -15,11 +14,6 @@ const questions = [
     name: "Description",
     message: "How would you describe your project?",
   
-  },
-  {
-    type: "input",
-    name: "Table of Contents?",
-    message: "How would you lay out your table of contents?",
   },
   {
     type: "input",
@@ -39,36 +33,64 @@ const questions = [
     message: "Enter your GitHub username",
   
   },
+  {
+    type: "input",
+    name: "License",
+    message: "Enter your license",
+  },
+  {
+    type: "input",
+    name: "Contributions",
+    message: "Who/what contributed to the completion of this project?",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "How can this app be tested?",
+  },
+  {
+    type: "input",
+    name: "Questions",
+    message: "Please enter your email address and explain any way in which you can be reached.",
+  },
 ];
 
 function askQuestions() {
-
-  inquirer.prompt(questions).then(res => {
-    console.log(res)
-    let textoutput = "# "
-    let toc = ""
-    let rx = new RegExp(/ /g)
-    for (let prop in res) {
-      if (prop !== 'readme title') {
-        textoutput += "## " + prop + "<br> \n" + res[prop] + "<br> \n"
-        toc += "["+prop+"](#"+prop.toLowerCase().replace(rx,"-")+") \n" 
-        // [Project Readme](#project-readme)
+  inquirer
+    .prompt(questions)
+    .then((res) => {
+      console.log(res);
+      let textoutput = "# ";
+      let toc = "";
+      let rx = new RegExp(/ /g);
+      for (let prop in res) {
+        console.log(prop)
+         if (prop === "License"){
+          textoutput += "[![Generic badge](https://img.shields.io/badge/License-"+res[prop]+"-GREEN.svg)](https://shields.io/)" 
+        } else if (prop !== "readme title") {
+          textoutput += "## " + prop + "<br> \n" + res[prop] + "<br> \n";
+          toc +=
+            "[" + prop + "](#" + prop.toLowerCase().replace(rx, "-") + ") \n";
         }
-        else {
-          textoutput += prop + "<br> \n" + res[prop] + "<br> \n"
+         else {
+          textoutput += prop + "<br> \n" + res[prop] + "<br> \n";
         }
-    }
-    console.log(textoutput)
-    return toc + textoutput
-  }).then(readmetext => {
-    fs.writeFile('readme.md', readmetext, err => console.log('readme.md saved!'))
-  }).catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+      }
+      console.log(textoutput);
+      return toc + textoutput;
+    })
+    .then((readmetext) => {
+      fs.writeFile("readme.md", readmetext, (err) =>
+        console.log("readme.md saved!")
+      );
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else when wrong
+      }
+    });
 }
 
 const answers = askQuestions()
